bbd9c71554f7fd123b3593aedcf01c08dd59981e
Vladislav <vladislav.west01@gmail.com>
2022-06-20 18:34:42 +0300

    oldmain для запуска первоначальной версии

---
+++ oldMain.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QApplication
+from MainWindow import *
+
+import sys
+
+
+def RunApplication():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    RunApplication()
